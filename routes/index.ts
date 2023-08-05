ecd1c9c3036d921f3f15f3f64de734b63d5174f2
Himesh9512 <himeshjangid0000@gmail.com>
2023-08-05 10:10:20 +0530

    Change in error handling

--- routes/index.ts
+++ routes/index.ts
@@ -9,26 +9,29 @@ interface MessageInterface {
 	user: string;
 	added: string;
 }
+try {
+	router.get("/", async function (req: Request, res: Response, next: NextFunction): Promise<void> {
+		const messages: MessageInterface[] = await Message.find();
+		res.render("index", { title: "Mini Message Board | Home", messages: messages });
+	});
 
-router.get("/", async function (req: Request, res: Response, next: NextFunction): Promise<void> {
-	const messages: MessageInterface[] = await Message.find();
-	res.render("index", { title: "Mini Message Board | Home", messages: messages });
-});
+	router.get("/new", function (req: Request, res: Response, next: NextFunction): void {
+		console.log(req.body);
+		res.render("form", { title: "Mini Message Board | New" });
+	});
 
-router.get("/new", function (req: Request, res: Response, next: NextFunction): void {
-	console.log(req.body);
-	res.render("form", { title: "Mini Message Board | New" });
-});
+	router.post(
+		"/new",
+		async function (req: Request, res: Response, next: NextFunction): Promise<void> {
+			const { username, message }: { username: string; message: string } = req.body;
+			const currentDate: string = moment().format("MMMM Do YYYY, h:mm a");
 
-router.post(
-	"/new",
-	async function (req: Request, res: Response, next: NextFunction): Promise<void> {
-		const { username, message }: { username: string; message: string } = req.body;
-		const currentDate: string = moment().format("MMMM Do YYYY, h:mm a");
-
-		await Message.create({ user: username, text: message, added: currentDate });
-		res.redirect("/");
-	},
-);
+			await Message.create({ user: username, text: message, added: currentDate });
+			res.redirect("/");
+		},
+	);
+} catch (e) {
+	console.log(e);
+}
 
 module.exports = router;
